b8490daaa023e87d9392918dbc5ad4055325a8d7
Bernhard Froehlich <decke@bluelife.at>
2020-12-29 12:36:10 +0000

    multimedia/kodi-addon-pvr-hts: Fix building

---
+++ multimedia/kodi-addon-pvr-hts/files/patch-src_tvheadend_entity_Event.h
@@ -0,0 +1,10 @@
+--- src/tvheadend/entity/Event.h.orig	2020-12-03 21:26:09 UTC
++++ src/tvheadend/entity/Event.h
+@@ -14,6 +14,7 @@
+ #include <map>
+ #include <string>
+ #include <vector>
++#include <time.h>
+ 
+ namespace tvheadend
+ {
